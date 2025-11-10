fd3f86b08c6085a681ab810b11d6e1135bdb6683
Rapro <raproplayer@seznam.cz>
2025-11-10 08:58:50 +0100

    made the projectiles do damage and kill enemies

--- waves-game/scenes/Scripts/enemy.gd
+++ waves-game/scenes/Scripts/enemy.gd
@@ -1,6 +1,21 @@
 extends CharacterBody2D
 
 @export var enemy_health := 5
+var dead = false
 
 func _process(_delta: float) -> void:
 	move_and_slide()
+	check_death()
+	
+	if dead == true:
+		queue_free()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	print("Hit enemy.")
+	
+	enemy_health -= 1
+
+func check_death():
+	if enemy_health <= 0:
+		dead = true
